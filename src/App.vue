99eee46aef5ef562cee4d1040c97c621ba78149c
YoungDan <1292867089@qq.com>
2022-08-18 18:18:43 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App1222"/>
+  <HelloWorld msg="Welcome to Your Vue.js App122222"/>
 </template>
 
 <script>
